eec5e630ff4899f00f62010286ca130392bb255d
Eugene <evgen29031999@bk.ru>
2021-12-21 22:25:48 +0300

    add borderLine but doesn't work

---
+++ SmartHome/Button.swift
@@ -0,0 +1,39 @@
+//
+//  Button.swift
+//  SmartHome
+//
+//  Created by Eugene on 21.12.2021.
+//
+
+import Foundation
+import UIKit
+
+enum UIButtonBorderSide{
+    case Top, Bottom, Left, Rigth
+}
+
+extension UIButton{
+    
+    func addBorder(side: UIButtonBorderSide, color: UIColor, width: CGFloat){
+        let border = CALayer()
+        
+        border.name = "borderLayer"
+        border.backgroundColor = color.cgColor
+        
+        switch side{
+            case .Top: border.frame = CGRect(x: 0, y: 0, width: self.frame.size.width, height: width)
+            case .Bottom: border.frame = CGRect(x: 0, y: self.frame.size.height - width, width: self.frame.size.width, height: width)
+            case .Left: border.frame = CGRect(x: 0, y: 0, width: width, height: self.frame.size.height)
+            case .Rigth: border.frame = CGRect(x: self.frame.size.width - width, y: 0, width: width, height: self.frame.size.height)
+        }
+    }
+    
+    func removeBorder(){
+        for item in self.layer.sublayers!{
+            if item.name == "borderLayer"{
+                item.removeFromSuperlayer()
+            }
+        }
+    }
+    
+}
--- SmartHome/Controllers/ViewController.swift
+++ SmartHome/Controllers/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CameraButton: UIButton!
+    @IBOutlet weak var DoorsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        CameraButton.addBorder(side: .Bottom, color: UIColor.red, width: 2.0)
+        DoorsButton.addBorder(side: .Bottom, color: UIColor.blue, width: 2.0)
+        
     }
 
-
+    
+    
+    
+    
 }
 
