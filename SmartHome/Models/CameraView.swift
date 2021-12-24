7cbfff8163bbfd0a015a02aefacda861870c8dd4
Eugene <evgen29031999@bk.ru>
2021-12-24 23:47:49 +0300

    add CameraView

---
+++ SmartHome/Models/CameraView.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  SmartHome
+//
+//  Created by Eugene on 24.12.2021.
+//
+
+import Foundation
+import UIKit
+
+struct CameraView{
+    var cameraView: UIImage
+    var icon: UIImage
+    var title: String
+    var detail: String
+}
+
