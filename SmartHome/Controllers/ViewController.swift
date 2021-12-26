2aef9804f3cca16e387274eae69db1512d9bdb7f
Eugene <evgen29031999@bk.ru>
2021-12-26 21:47:09 +0300

    add reload for table and add new array of data

--- SmartHome/Controllers/ViewController.swift
+++ SmartHome/Controllers/ViewController.swift
@@ -13,43 +13,84 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var CameraButton: UIButton!
     @IBOutlet weak var DoorsButton: UIButton!
     @IBOutlet weak var mainCamerasTitle: UILabel!
+    @IBOutlet weak var mainTableView: UITableView!
+    
+    var currentWindow = 1
     
     var camViews: [CameraView] = [
-        CameraView(cameraView: "", icon: "icon", title: "Title", detail: "detail"),
+        CameraView(cameraView: "sunrise", icon: "", title: "Title", detail: ""),
         CameraView(cameraView: "sunrise", icon: "", title: "Title", detail: "")
+    ]
+    
+    var doorWindow: [CameraView] = [
+        CameraView(cameraView: "", icon: "lock", title: "Подъезд 1", detail: ""),
+        CameraView(cameraView: "", icon: "lock", title: "Выход на пожарную лестницу", detail: ""),
+        CameraView(cameraView: "", icon: "lock", title: "Подъезд 2", detail: ""),
+        CameraView(cameraView: "sunrise", icon: "lock", title: "Домофон", detail: "В сети")
     ]
 
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return camViews.count
+        
+        if currentWindow == 1{
+            return camViews.count
+        }else{
+            return doorWindow.count
+        }
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "firstTypeCell", for: indexPath) as! FirstTypeTableViewCell
         
-        
-        if !camViews[indexPath.row].cameraView!.isEmpty{
-            cell.imageCamera.image = UIImage(named: camViews[indexPath.row].cameraView!)
-        }else{
-            cell.imageCamera.isHidden = true
-        }
-        
-        if !camViews[indexPath.row].icon!.isEmpty{
-            cell.iconCamera.image = UIImage(named: camViews[indexPath.row].icon!)
-        }else{
-            cell.iconCamera.isHidden = true
-        }
-        
-        if !camViews[indexPath.row].title!.isEmpty{
-            cell.title.text = camViews[indexPath.row].title
-        }else{
-            cell.title.isHidden = true
-        }
-        
-        if !camViews[indexPath.row].detail!.isEmpty{
-            cell.detail.text = camViews[indexPath.row].detail
-        }else{
-            cell.detail.isHidden = true
+        if currentWindow == 1{
+            if !camViews[indexPath.row].cameraView!.isEmpty{
+                cell.imageCamera.image = UIImage(named: camViews[indexPath.row].cameraView!)
+            }else{
+                cell.imageCamera.isHidden = true
+            }
+            
+            if !camViews[indexPath.row].icon!.isEmpty{
+                cell.iconCamera.image = UIImage(named: camViews[indexPath.row].icon!)
+            }else{
+                cell.iconCamera.isHidden = true
+            }
+            
+            if !camViews[indexPath.row].title!.isEmpty{
+                cell.title.text = camViews[indexPath.row].title
+            }else{
+                cell.title.isHidden = true
+            }
+            
+            if !camViews[indexPath.row].detail!.isEmpty{
+                cell.detail.text = camViews[indexPath.row].detail
+            }else{
+                cell.detail.isHidden = true
+            }
+        }else if currentWindow == 2{
+            if !doorWindow[indexPath.row].cameraView!.isEmpty{
+                cell.imageCamera.image = UIImage(named: doorWindow[indexPath.row].cameraView!)
+            }else{
+                cell.imageCamera.isHidden = true
+            }
+            
+            if !doorWindow[indexPath.row].icon!.isEmpty{
+                cell.iconCamera.image = UIImage(named: doorWindow[indexPath.row].icon!)
+            }else{
+                cell.iconCamera.isHidden = true
+            }
+            
+            if !doorWindow[indexPath.row].title!.isEmpty{
+                cell.title.text = doorWindow[indexPath.row].title
+            }else{
+                cell.title.isHidden = true
+            }
+            
+            if !doorWindow[indexPath.row].detail!.isEmpty{
+                cell.detail.text = doorWindow[indexPath.row].detail
+            }else{
+                cell.detail.isHidden = true
+            }
         }
         
         cell.selectionStyle = .none
@@ -61,15 +102,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return 1
     }
     
-    var currentButton = 1
-    
     @IBAction func CamButtonAction(_ sender: Any) {
-        currentButton = 1
+        currentWindow = 1
         mainCamerasTitle.isHidden = false
         changeCurrentWindow()
     }
     @IBAction func DoorButtonAction(_ sender: Any) {
-        currentButton = 2
+        currentWindow = 2
         mainCamerasTitle.isHidden = true
         changeCurrentWindow()
     }
@@ -81,15 +120,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func changeCurrentWindow(){
-        if (currentButton == 1){
+        if (currentWindow == 1){
             CameraButton.addBorder(side: .Bottom, color: UIColor.init(named: "borderColor")!, width: 2)
             DoorsButton.removeBorder()
         }
             
-        if (currentButton == 2){
+        if (currentWindow == 2){
             DoorsButton.addBorder(side: .Bottom, color: UIColor.init(named: "borderColor")!, width: 2)
             CameraButton.removeBorder()
         }
+        
+        mainTableView.reloadData()
     }
     
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
@@ -108,7 +149,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         rate.image = UIImage(named: "star")
         rate.backgroundColor = UIColor(named: "mainBG")
         
-        
+        if currentWindow == 1{
+            return UISwipeActionsConfiguration(actions: [rate])
+        }
         
         return UISwipeActionsConfiguration(actions: [rate,edit])
     }
